d7427cfbabaacb00eb2908a8a92aa9e160f28eeb
2568832 <2568832@cegepmontpetit.ca>
2026-04-08 15:39:11 -0400

    ServiceGuichet presque terminer

--- TP2_AppGuichet_Materiel/Models/ServiceGuichet.cs
+++ TP2_AppGuichet_Materiel/Models/ServiceGuichet.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Media.Protection.PlayReady;
 
 namespace Models
 {
@@ -29,18 +30,24 @@ namespace Models
             get;
         }
 
-        public List<Client> Clients { get; }
+        public List<Client> Clients { get;}
 
         public List<Transaction> Transactions { get; }
 
 
         // Constructeur
-        ServiceGuichet(string pCheminFichierClients, string pCheminFichierTransactions)
+        public ServiceGuichet(string pCheminFichierClients, string pCheminFichierTransactions)
         {
             if (File.Exists(pCheminFichierClients) || File.Exists(pCheminFichierTransactions))
             {
                 CheminFichierClients = pCheminFichierClients;
                 CheminFichierTransactions = pCheminFichierTransactions;
+                ChargerClients();
+                ChargerTransactions();
+            }
+            else
+            {
+                throw new ArgumentException();
             }
             
 
@@ -95,9 +102,7 @@ namespace Models
             
             try
             {
-                Client c = new Client(pNumClient);
-                
-                if (TrouverClient(c))
+                if (TrouverClient(pNumClient) == null)
                 {
                     Transaction T = new Transaction(pSorte, pNumClient, pDate, pMontant);
                     Clients[int.Parse(pNumClient)].AjouterTransaction(T);
@@ -113,29 +118,95 @@ namespace Models
 
         public bool Sauvegarde()
         {
-           
+            try {
+                SauvegarderClients(CheminFichierClients);
+                SauvegarderTransactions(CheminFichierTransactions);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public void SauvegarderTransactions(string pFichier)
         {
+            
+            using (StreamWriter writ = new StreamWriter(pFichier))
+            {
 
+                for (int i = 0; i < Transactions.Count; i++)
+                {
+                    Transaction t = new Transaction(Transactions[i].SorteTransaction, Transactions[i].NumClient, Transactions[i].Date, Transactions[i].Montant);
+                    string T = t.ToCsv();
+                    writ.WriteLine(T);
+                    
+                }
+
+            }
+            
+            
+        }
+        public void SauvegarderClients(string pFichier)
+        {
+            using (StreamWriter writ = new StreamWriter(pFichier))
+            {
+
+                for (int i = 0; i < Clients.Count; i++)
+                {
+                    Client client = new Client(Clients[i].NumClient, Clients[i].Nom, Clients[i].MotDePasse, Clients[i].Role, Clients[i].SorteCompte, Clients[i].Solde);
+                    string C = client.ToCsv();
+                    writ.WriteLine(C);
+
+                }
+
+            }
         }
         public bool Connexion(string numClient, string motDePasse)
         {
-            
+            bool rep = false;
+            foreach (Client C in Clients)
+            {
+                if (C.NumClient == numClient && C.MotDePasse == motDePasse)
+                {
+                    ClientCourant = C;
+                    rep = true;
+                    break;
+                }
+                else if (rep == false)
+                {
+                    rep = false;
+
+                }
+                
+
+            }
+            return rep;
         }
 
         public bool Deconnexion()
         {
-           
+            ClientCourant = null;
+            return true;
         }
 
         public Client TrouverClient(string pNumClient)
         {
-           
+            Client Client = null;
+            foreach (Client C in Clients)
+            {
+                if (C.NumClient == pNumClient)
+                {
+                    Client = C;
+                    break;
+                }
+                else
+                {
+                    Client = null;
+
+                }
+            }
+            return Client;
         }
     }
-
-
-
 }
